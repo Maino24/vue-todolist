7d3082de5059dbfe16ed6a88f10536cfe91e03bc
Maino24 <mg02.gabriele@gmail.com>
2022-11-14 17:35:42 +0100

    creato array di oggetti

---
+++ main.js
@@ -0,0 +1,37 @@
+var app = new Vue ({
+    el: `app`,
+
+    data: {
+
+        todo: [{
+
+            todo1: {
+                text: `fare esercizio`,
+                stato: ``,
+            },
+            todo2: {
+                text: ``,
+                stato: ``,
+            },
+            todo3: {
+                text: ``,
+                stato: ``,
+            },
+            todo4: {
+                text: ``,
+                stato: ``,
+            },
+
+
+        }]
+        
+    }
+
+    
+
+
+
+
+
+
+})
